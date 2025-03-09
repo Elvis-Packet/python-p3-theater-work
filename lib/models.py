60f78fd7521b3b496b67d7275f2412c388c9ae62
Elvis Mbugua <manfuturekahande@gmail.com>
2025-03-09 23:32:14 +0300

    code

--- lib/models.py
+++ lib/models.py
@@ -1,10 +1,50 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
-convention = {
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-}
-metadata = MetaData(naming_convention=convention)
+Base = declarative_base()
 
-Base = declarative_base(metadata=metadata)
+class Audition(Base):
+    __tablename__ = 'auditions'
+
+    id = Column(Integer, primary_key=True)
+    actor = Column(String)
+    location = Column(String)
+    phone = Column(String)
+    hired = Column(Boolean, default=False)
+    role_id = Column(Integer, ForeignKey('roles.id'))
+
+    role = relationship('Role', back_populates='auditions')
+
+    def call_back(self):
+        self.hired = True
+
+class Role(Base):
+    __tablename__ = 'roles'
+
+    id = Column(Integer, primary_key=True)
+    character_name = Column(String)
+
+    auditions = relationship('Audition', back_populates='role')
+
+    def actors(self):
+        return [audition.actor for audition in self.auditions]
+
+    def locations(self):
+        return [audition.location for audition in self.auditions]
+
+    def lead(self):
+        hired_auditions = [audition for audition in self.auditions if audition.hired]
+        if not hired_auditions:
+            return 'no actor has been hired for this role'
+        hired_auditions_sorted = sorted(hired_auditions, key=lambda a: a.id)
+        return hired_auditions_sorted[0]
+
+    def understudy(self):
+        hired_auditions = [audition for audition in self.auditions if audition.hired]
+        if len(hired_auditions) < 2:
+            return 'no actor has been hired for understudy for this role'
+        hired_auditions_sorted = sorted(hired_auditions, key=lambda a: a.id)
+        return hired_auditions_sorted[1]
+
+
