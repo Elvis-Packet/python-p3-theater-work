b216af86b9a5e1888cdcfc6fbdfbd97635366ba6
Elvis-Packet <manfuturekahande@gmail.com>
2025-03-13 11:08:56 +0300

    Updated models.py with latest changes

--- lib/models.py
+++ lib/models.py
@@ -1,50 +1,57 @@
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
-from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.declarative import declarative_base 
+from sqlalchemy.orm import relationship, sessionmaker 
+from sqlalchemy import Column, String, Integer, ForeignKey, Boolean, create_engine 
 
 Base = declarative_base()
+
 
 class Audition(Base):
     __tablename__ = 'auditions'
 
     id = Column(Integer, primary_key=True)
-    actor = Column(String)
-    location = Column(String)
-    phone = Column(String)
+    actor = Column(String, nullable=False)
+    location = Column(String, nullable=False)
+    phone = Column(Integer, nullable=False)
     hired = Column(Boolean, default=False)
-    role_id = Column(Integer, ForeignKey('roles.id'))
-
+    
+    role_id = Column(Integer, ForeignKey('roles.id'), nullable=False)
     role = relationship('Role', back_populates='auditions')
+    
+    def __repr__(self) -> str:
+        return f"Audition(id={self.id}, actor={self.actor}, location={self.location}, phone={self.phone}, hired={self.hired}, role={self.role})"
 
-    def call_back(self):
+    def call_back(self) -> None:
         self.hired = True
-
 class Role(Base):
+    
     __tablename__ = 'roles'
 
     id = Column(Integer, primary_key=True)
-    character_name = Column(String)
+    character_name = Column(String, nullable=False)
 
-    auditions = relationship('Audition', back_populates='role')
-
-    def actors(self):
+    auditions = relationship('Audition', back_populates='role', cascade='all, delete')
+    
+    def __repr__(self) -> str:
+        return f"Role(id={self.id}, character_name={self.character_name})"
+    
+    def actors(self) -> list:
         return [audition.actor for audition in self.auditions]
-
-    def locations(self):
+    
+    def locations(self) -> list:
         return [audition.location for audition in self.auditions]
-
+    
     def lead(self):
-        hired_auditions = [audition for audition in self.auditions if audition.hired]
-        if not hired_auditions:
-            return 'no actor has been hired for this role'
-        hired_auditions_sorted = sorted(hired_auditions, key=lambda a: a.id)
-        return hired_auditions_sorted[0]
-
+        for audition in self.auditions:
+            if audition.hired:
+                return audition
+        return "no actor has been hired for this role"
+    
     def understudy(self):
-        hired_auditions = [audition for audition in self.auditions if audition.hired]
-        if len(hired_auditions) < 2:
-            return 'no actor has been hired for understudy for this role'
-        hired_auditions_sorted = sorted(hired_auditions, key=lambda a: a.id)
-        return hired_auditions_sorted[1]
+        hired_actors = [audition.actor for audition in self.auditions if audition.hired]
+        return hired_actors[1] if len(hired_actors) > 1 else "no actor has been hired for understudy for this role"
 
+engine = create_engine('sqlite:///theater.db')
+Base.metadata.create_all(engine)
 
+Session = sessionmaker(bind=engine)
+session = Session()
